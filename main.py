0479d6e5d5f166549b2c069e0f1aec75ad7a3b68
icode <jgentile29@setoncatholic.org>
2026-09-23 20:14:23 +0000

    calc

---
+++ main.py
@@ -0,0 +1,27 @@
+#this function adds two numbers
+def add(x,y):
+    print(x+y)
+
+
+#this function sub two numbers
+def sub(x,y):
+    print(x-y)
+
+#this function mult two numbers
+def mult(x,y):
+    print(x*y)
+
+
+#this function div two numbers
+def div(x,y):
+    print(x/y)
+
+
+x=int(input("enter ur first number: "))
+y=int(input( "enter your second muber: "))
+
+
+add(x,y)
+sub(x,y)
+mult(x,y)
+div(x,y)
